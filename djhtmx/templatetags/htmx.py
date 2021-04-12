fcf7452649cb4141edc1cdf320bb81bdd17039f8
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2021-04-12 22:10:27 +0200

    Rename htmx_header to htmx-header

--- djhtmx/templatetags/htmx.py
+++ djhtmx/templatetags/htmx.py
@@ -15,7 +15,7 @@ register = template.Library()
 CSRF_HEADER = settings.CSRF_HEADER_NAME[5:].replace('_', '-')
 
 
-@register.inclusion_tag('htmx/headers.html')
+@register.inclusion_tag('htmx/headers.html', name='htmx-header')
 def htmx_headers():
     """Loads all the necessary scripts to make this work
 
