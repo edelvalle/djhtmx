45afc7cd38003f28c3e7e12f4fa159ee971f4620
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2021-04-14 12:31:38 +0200

    If the target is the document replace it directly

--- djhtmx/static/htmx/htmx-django.js
+++ djhtmx/static/htmx/htmx-django.js
@@ -4,41 +4,46 @@ htmx.defineExtension('morphdom-swap', {
   },
   handleSwap: function (swapStyle, target, fragment) {
     if (swapStyle === 'morphdom') {
-      morphdom(
-        target,
-        fragment.outerHTML,
-        {
-          onBeforeElUpdated: (fromEl, toEl) => {
-            if (fromEl.hasAttribute(':once')) {
-              return false;
-            }
-            const tagName= fromEl.tagName;
-            const shouldPatch = (
-              fromEl === document.activeElement &&
-              (tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA') &&
-              !fromEl.hasAttribute(':override')
-            )
-            if (shouldPatch) {
-              toEl.getAttributeNames().forEach((name) =>
-                fromEl.setAttribute(name, toEl.getAttribute(name))
-              );
-              fromEl.readOnly = toEl.readOnly;
-              return false;
-            }
-            return true;
-          },
-          onElUpdated: (el) => {
-            code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
-            if (code) (() => { eval(code) }).bind(el)();
-          },
+      if (target.nodeName === 'BODY') {
+        morphdom(target, fragment);
+        return [target];
+      } else {
+        morphdom(
+          target,
+          fragment.outerHTML,
+          {
+            onBeforeElUpdated: (fromEl, toEl) => {
+              if (fromEl.hasAttribute(':once')) {
+                return false;
+              }
+              const tagName= fromEl.tagName;
+              const shouldPatch = (
+                fromEl === document.activeElement &&
+                (tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA') &&
+                !fromEl.hasAttribute(':override')
+              )
+              if (shouldPatch) {
+                toEl.getAttributeNames().forEach((name) =>
+                  fromEl.setAttribute(name, toEl.getAttribute(name))
+                );
+                fromEl.readOnly = toEl.readOnly;
+                return false;
+              }
+              return true;
+            },
+            onElUpdated: (el) => {
+              code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
+              if (code) (() => { eval(code) }).bind(el)();
+            },
 
-          onNodeAdded: (el) => {
-            code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
-            if (code) (() => { eval(code) }).bind(el)();
-          },
-        }
-      );
-      return [target];
+            onNodeAdded: (el) => {
+              code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
+              if (code) (() => { eval(code) }).bind(el)();
+            },
+          }
+        );
+        return [target];
+      }
     }
   },
 });
