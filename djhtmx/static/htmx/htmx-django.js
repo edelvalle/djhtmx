5775ca66d6503af422e577a594bac60c542406f7
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2021-04-14 12:44:10 +0200

    Execute JS hooks when navigating too.

--- djhtmx/static/htmx/htmx-django.js
+++ djhtmx/static/htmx/htmx-django.js
@@ -1,3 +1,38 @@
+const DEFAULT_MORPHDOM_OPTIONS = {
+  onElUpdated: (el) => {
+    // Executes JS hooks
+    code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
+    if (code) (() => { eval(code) }).bind(el)();
+  },
+
+  onNodeAdded: (el) => {
+    // Executes JS hooks
+    code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
+    if (code) (() => { eval(code) }).bind(el)();
+  },
+};
+
+const INTERACTIVE_MORHDOM_OPTIONS = Object.assign({}, DEFAULT_MORPHDOM_OPTIONS, {
+   onBeforeElUpdated: (fromEl, toEl) => {
+    // Keeps the focus on an input if it was focused before replacement
+    const tagName= fromEl.tagName;
+    const shouldPatch = (
+      fromEl === document.activeElement &&
+      (tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA') &&
+      !fromEl.hasAttribute(':override')
+    )
+    if (shouldPatch) {
+      toEl.getAttributeNames().forEach((name) =>
+        fromEl.setAttribute(name, toEl.getAttribute(name))
+      );
+      fromEl.readOnly = toEl.readOnly;
+      return false;
+    }
+    return true;
+  },
+});
+
+
 htmx.defineExtension('morphdom-swap', {
   isInlineSwap: function(swapStyle) {
     return swapStyle === 'morphdom';
@@ -5,45 +40,11 @@ htmx.defineExtension('morphdom-swap', {
   handleSwap: function (swapStyle, target, fragment) {
     if (swapStyle === 'morphdom') {
       if (target.nodeName === 'BODY') {
-        morphdom(target, fragment);
-        return [target];
+        morphdom(target, fragment, DEFAULT_MORPHDOM_OPTIONS);
       } else {
-        morphdom(
-          target,
-          fragment.outerHTML,
-          {
-            onBeforeElUpdated: (fromEl, toEl) => {
-              if (fromEl.hasAttribute(':once')) {
-                return false;
-              }
-              const tagName= fromEl.tagName;
-              const shouldPatch = (
-                fromEl === document.activeElement &&
-                (tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA') &&
-                !fromEl.hasAttribute(':override')
-              )
-              if (shouldPatch) {
-                toEl.getAttributeNames().forEach((name) =>
-                  fromEl.setAttribute(name, toEl.getAttribute(name))
-                );
-                fromEl.readOnly = toEl.readOnly;
-                return false;
-              }
-              return true;
-            },
-            onElUpdated: (el) => {
-              code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
-              if (code) (() => { eval(code) }).bind(el)();
-            },
-
-            onNodeAdded: (el) => {
-              code = typeof el.getAttribute === "function" ? el.getAttribute('hx-updated') : void 0;
-              if (code) (() => { eval(code) }).bind(el)();
-            },
-          }
-        );
-        return [target];
+        morphdom(target, fragment.outerHTML, INTERACTIVE_MORHDOM_OPTIONS);
       }
+      return [target];
     }
   },
 });
