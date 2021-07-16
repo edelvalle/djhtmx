7ff6ba5f909a2700d000da675cd65dc0dd362d7f
Manuel Vázquez Acosta <manuel@merchise.org>
2021-07-16 08:05:55 +0200

    Correct (if needed/possible) the transaction name in Sentry.
    When Sentry is configured using the standard integration it will collapse all
    HTMX transactions under the same transaction name, e.g
    '/_htmx/{component_name}/{event_handler}'; however, this makes harder to
    identify performance problems in specific components.

--- djhtmx/urls.py
+++ djhtmx/urls.py
@@ -1,3 +1,5 @@
+import contextlib
+
 from django.core.signing import Signer
 from django.urls import path
 
@@ -5,17 +7,40 @@ from . import json
 from .component import Component
 from .introspection import filter_parameters, parse_request_data
 
+try:
+    from sentry_sdk import configure_scope
+
+    @contextlib.contextmanager
+    def sentry_transaction(request, component_name, event_handler):
+        transaction = f"HTMX {request.method} {component_name}.{event_handler}"
+        with configure_scope() as scope:
+            # XXX: The docs says we should scope.transaction, but when I do
+            # that, the transaction keeps the old name (URL).
+            try:
+                scope.transaction.name = transaction
+            except Exception:
+                scope.transaction = transaction
+            yield
+
+
+except ImportError:
+
+    @contextlib.contextmanager
+    def sentry_transaction(request, component_name, event_handler):
+        yield
+
 
 def endpoint(request, component_name, event_handler):
-    id = request.META.get('HTTP_HX_TARGET')
-    state = request.META.get('HTTP_X_COMPONENT_STATE', '')
-    state = Signer().unsign(state)
-    state = json.loads(state)
-    component = Component._build(component_name, request, id, state)
-    handler = getattr(component, event_handler)
-    handler_kwargs = parse_request_data(request)
-    handler_kwargs = filter_parameters(handler, handler_kwargs)
-    return handler(**handler_kwargs) or component.render()
+    with sentry_transaction(request, component_name, event_handler):
+        id = request.META.get('HTTP_HX_TARGET')
+        state = request.META.get('HTTP_X_COMPONENT_STATE', '')
+        state = Signer().unsign(state)
+        state = json.loads(state)
+        component = Component._build(component_name, request, id, state)
+        handler = getattr(component, event_handler)
+        handler_kwargs = parse_request_data(request)
+        handler_kwargs = filter_parameters(handler, handler_kwargs)
+        return handler(**handler_kwargs) or component.render()
 
 
 urlpatterns = [
