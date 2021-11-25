e1ec266da07e2158cf796db2794398072b8bc097
Manuel Vázquez Acosta <manuel@merchise.org>
2021-11-25 11:27:08 +0100

    Fix script URL to our integration script with Django's CSRF token.

--- djhtmx/static/htmx/htmx-django.js
+++ djhtmx/static/htmx/htmx-django.js
@@ -1,36 +1,38 @@
-function _runHxAfterSwap(element) {
-  const code = element.getAttribute('hx-after-swap');
-  if (code) (() => eval(code)).bind(element)();
-}
+(function() {
+    function _runHxAfterSwap(element) {
+        const code = element.getAttribute('hx-after-swap');
+        if (code) (() => eval(code)).bind(element)();
+    }
 
 
-document.body.addEventListener('htmx:afterSwap', (event) => {
-  _runHxAfterSwap(event.detail.target);
-  event.detail.target
-    .querySelectorAll('[hx-after-swap]')
-    .forEach(_runHxAfterSwap);
-});
+    document.body.addEventListener('htmx:afterSwap', (event) => {
+        _runHxAfterSwap(event.detail.target);
+        event.detail.target
+            .querySelectorAll('[hx-after-swap]')
+            .forEach(_runHxAfterSwap);
+    });
 
 
-document.body.addEventListener('htmx:configRequest', (event) => {
-  const csrf_header = document
-    .querySelector('meta[name=djang-csrf-header-name]')
-    .getAttribute('content');
-  const csrf_token = document
-    .querySelector('meta[name=djang-csrf-token]')
-    .getAttribute('content');
-  event.detail.headers[csrf_header] = csrf_token;
-});
+    document.body.addEventListener('htmx:configRequest', (event) => {
+        const csrf_header = document
+            .querySelector('meta[name=djang-csrf-header-name]')
+            .getAttribute('content');
+        const csrf_token = document
+            .querySelector('meta[name=djang-csrf-token]')
+            .getAttribute('content');
+        event.detail.headers[csrf_header] = csrf_token;
+    });
 
-document.addEventListener('hxSendEvent', (event) => {
-  event.detail.value.map(({event, target}) => {
-    document.querySelector(target).dispatchEvent(new Event(event));
-  });
-});
+    document.addEventListener('hxSendEvent', (event) => {
+        event.detail.value.map(({ event, target }) => {
+            document.querySelector(target).dispatchEvent(new Event(event));
+        });
+    });
 
 
-document.addEventListener('hxFocus', (event) => {
-  event.detail.value.map((selector) => {
-    document.querySelector(selector).focus();
-  });
-});
+    document.addEventListener('hxFocus', (event) => {
+        event.detail.value.map((selector) => {
+            document.querySelector(selector).focus();
+        });
+    });
+})();
