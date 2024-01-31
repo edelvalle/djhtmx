35f6819f51938702dd4513fcc1c0d7f9ebdc3448
Eddy del Valle Pino <eddy@edelvalle.me>
2024-01-31 10:45:29 +0100

    Do not use static when configuring the package

--- djhtmx/settings.py
+++ djhtmx/settings.py
@@ -1,6 +1,4 @@
 from django.conf import settings
-from django.templatetags.static import static
-
 
 VERSION = "1.9.10"
 DEBUG = settings.DEBUG
@@ -8,7 +6,6 @@ CSRF_HEADER_NAME = settings.CSRF_HEADER_NAME[5:].replace("_", "-")
 LOGIN_URL = settings.LOGIN_URL
 
 SCRIPT_URLS = [
-    static(f"htmx/{VERSION}/htmx{'.min' if DEBUG else ''}.js"),
-    static("htmx/django.js"),
+    f"htmx/{VERSION}/htmx{'.min' if DEBUG else ''}.js",
+    "htmx/django.js",
 ]
-
