487388e79ae89278c8a02d148d6d56d3d50fb5dc
Manuel Vázquez Acosta <m.vazquez@kaikosystems.com>
2024-05-13 07:09:14 +0200

    Fix improper state in legacy components.
    During the refactor we accidentally removed the call to '.unsign' in all
    legacy components.

--- src/djhtmx/urls.py
+++ src/djhtmx/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from djhtmx.executor import Executor
-
 from . import json
 from .component import REGISTRY, Component
+from .executor import Executor, signer
 from .introspection import filter_parameters, parse_request_data
 from .tracing import sentry_request_transaction
 
@@ -13,15 +12,22 @@ def endpoint(request, component_name, component_id, event_handler):
         if component_name in REGISTRY:
             # PydanticComponent
             executor = Executor(
-                request, component_name, component_id, event_handler
+                request,
+                component_name,
+                component_id,
+                event_handler,
             )
             return executor()
         else:
             # Legacy Component
-            state = request.META.get("HTTP_X_COMPONENT_STATE", "")
+            state = request.META.get("HTTP_X_COMPONENT_STATE", "{}")
+            state = signer.unsign(state)
             state = json.loads(state)
             component = Component._build(
-                component_name, request, component_id, state
+                component_name,
+                request,
+                component_id,
+                state,
             )
             handler = getattr(component, event_handler)
             handler_kwargs = parse_request_data(request.POST)
