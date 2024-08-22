4b00f75bfb543a14dcdc0a7d7be473d5677a5131
Eddy del Valle Pino <eddy@edelvalle.me>
2024-08-22 16:21:44 +0200

    Upgrade to HTMX 2.0.2

--- src/djhtmx/settings.py
+++ src/djhtmx/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-VERSION = "1.9.12"
+VERSION = "2.0.2"
 DEBUG = settings.DEBUG
 CSRF_HEADER_NAME = settings.CSRF_HEADER_NAME[5:].replace("_", "-")
 LOGIN_URL = settings.LOGIN_URL
