acbb21741315fe0047ed621a12e679e7aecea45c
Eddy del Valle Pino <eddy@edelvalle.me>
2024-09-11 08:50:33 +0200

    Remove deprecated middleware from test project

--- src/tests/fision/settings.py
+++ src/tests/fision/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "djhtmx.Middleware",
 ]
 
 ROOT_URLCONF = "fision.urls"
